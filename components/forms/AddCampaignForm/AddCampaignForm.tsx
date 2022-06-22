e5efbbbf430746fbf5ea14f2d3a83552d65cac48
Alexey <algoritm211@gmail.com>
2022-06-23 00:11:46 +0300

    Add deploying new Campaign

--- components/forms/AddCampaignForm/AddCampaignForm.tsx
+++ components/forms/AddCampaignForm/AddCampaignForm.tsx
@@ -1,24 +1,35 @@
-import {Button, Grid, TextField} from '@mui/material';
+import {Button, Grid, InputAdornment, TextField} from '@mui/material';
 import React from 'react';
 import {Field, Form } from 'react-final-form';
 import {requiredValidator} from "../../validators/requiredValidator";
 import AddIcon from "@mui/icons-material/Add";
 import {numberValidator} from "../../validators/numberValidator";
 import {composeValidators} from "../../validators/composeValidators";
+import web3 from "../../../ethereum/web3";
+import {campaignFactory} from "../../../ethereum/factory";
 
 interface FormProps {
   minimumContribution: number;
 }
 
 const AddCampaignForm: React.FC = () => {
-  const onSubmit = (values: FormProps) => {
-    console.log('FormValues', values)
+
+  const onSubmit = async (values: FormProps) => {
+    const accounts = await web3.eth.getAccounts();
+
+    await campaignFactory.methods.deployNewCampaign(values.minimumContribution)
+      .send({
+        from: accounts[0],
+      })
+
+    const deployedCampaigns = await campaignFactory.methods.getDeployedCampaigns().call();
+    console.log(deployedCampaigns);
   }
 
   return (
     <Form<FormProps>
       onSubmit={onSubmit}
-      render={({handleSubmit}) => {
+      render={({handleSubmit, submitting}) => {
         return (
           <form onSubmit={handleSubmit}>
             <Grid container flexDirection="column" alignItems="center" gap={2}>
@@ -33,6 +44,9 @@ const AddCampaignForm: React.FC = () => {
                         {...input}
                         error={touched && error}
                         label="Minimum Value"
+                        InputProps={{
+                          endAdornment: <InputAdornment position="end">WEI</InputAdornment>
+                        }}
                         helperText={touched && error}
                       />
                     )
@@ -40,7 +54,11 @@ const AddCampaignForm: React.FC = () => {
                 />
               </Grid>
               <Grid>
-                <Button variant="contained" type="submit" startIcon={<AddIcon />}>
+                <Button
+                  variant="contained"
+                  type="submit"
+                  disabled={submitting}
+                  startIcon={<AddIcon />}>
                   Add campaign
                 </Button>
               </Grid>
